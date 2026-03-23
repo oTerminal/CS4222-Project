32a67c400829cca914496d7c1daf132575e43371
Vaibhav Prakash <pvaibhav2007@outlook.com>
2026-03-23 13:13:42 +0000

    base files

---
+++ SillyGuitar.java
@@ -0,0 +1,10 @@
+import javax.sound.midi.*;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+public class SillyGuitar extends JFrame {
+    public static void main(String[] args){
+        System.out.println("Hello World.");
+    }
+}
--- shitInstrument.java
+++
@@ -1,5 +0,0 @@
-public class shitInstrument {
-    public static void main(String[] args){
-        System.out.println("Hello World.");
-    }
-}
