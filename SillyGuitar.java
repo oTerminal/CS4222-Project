19e3bb49637326e69c7c5e5f9a4d412a972fb430
Suhas <suhaskalyan2351@gmail.com>
2026-03-25 11:12:32 +0000

    test message

--- SillyGuitar.java
+++ SillyGuitar.java
@@ -15,7 +15,7 @@ public class SillyGuitar {
     public static class ScreenManager extends JPanel{
         CardLayout cardLayout;
         SplashScreen splashScreen;
-        GuitarScreen guitarScreen;
+        GuitarScreen guitarScreen; //sigma
 
         ScreenManager() {
             cardLayout = new CardLayout();
